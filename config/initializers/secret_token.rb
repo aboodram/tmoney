a68e4027adc21b9a1a9427e735a56579ecd05498
TaylorAllred <tay.allred@gmail.com>
2013-07-14 19:32:21 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tmoney::Application.config.secret_key_base = '0d1278e446cf5d20181f76c75d05bc556d50f69853718d82428a64ea34e3f42d7e5c5de6e74a480a875105e334b8930451d29ba56ec22d99e08b2b41f1ab6822'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tmoney::Application.config.session_store :cookie_store, key: '_tmoney_session'
